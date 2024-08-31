436c308b33e5e8ba274632fb1dfbf148496b4edc
Shaliha07 <119658046+Shaliha07@users.noreply.github.com>
2024-08-31 19:23:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/users.js";
+import employeeRoutes from "./routes/employees.js";
+import categoryRoutes from "./routes/categories.js";
+import goodRoutes from "./routes/goods.js";
+import serviceRoutes from "./routes/services.js";
+import appointmentRoutes from "./routes/appointments.js";
+import salesOrderRoutes from "./routes/salesorders.js";
+import salesOrderDetailsRoutes from "./routes/salesorderdetails.js";
 
 // Create an Express application
 const app = express();
@@ -38,9 +45,16 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/users", userRoutes);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/goods", goodRoutes);
+app.use("/api/service", serviceRoutes);
+app.use("/api/appointment", appointmentRoutes);
+app.use("/api/salesorder", salesOrderRoutes);
+app.use("/api/salesorderdetails", salesOrderDetailsRoutes);
 
 // Start the server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
