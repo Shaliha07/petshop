04d2b64b11041c7b2dd5a28f69e445d5d563c5e3
Shaliha07 <119658046+Shaliha07@users.noreply.github.com>
2024-09-09 22:21:51 +0530

    Update categories.js

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -6,13 +6,14 @@ import {
   deleteCategory,
   createCategory,
 } from "../controllers/category.js";
+import { authRole } from "../middleware/authRole.js";
 
 const router = express.Router();
 
-router.post("/create", createCategory);
-router.put("/:id", updateCategory);
+router.post("/create",authRole("admin"), createCategory)
+router.put("/:id",authRole("admin"), updateCategory);
 router.get("/:id", getCategory);
 router.get("/", getCategories);
-router.delete("/:id", deleteCategory);
+router.delete("/:id", authRole("admin"),deleteCategory);
 
 export default router;
